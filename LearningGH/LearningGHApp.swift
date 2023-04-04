fbf0b013d0c8c4dbb45e71048935fd47f5d7aeb1
Eloi Taha <eloitaha@yahoo.com>
2023-04-03 19:10:49 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Eloi Taha on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
